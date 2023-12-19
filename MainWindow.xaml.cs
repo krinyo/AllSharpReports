96d6c701f759f0c9f76f56a454b041c73b442f35
krea <krea@PC-IT-DEV>
2023-12-19 11:33:12 +0500

    first version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace AllSharpReports
 {
@@ -19,6 +14,35 @@ namespace AllSharpReports
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = $"Server={txtServerAddress.Text};Database={txtDatabaseName.Text};User ID={txtUsername.Text};Password={txtPassword.Password};";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(txtSqlQuery.Text, connection))
+                    {
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            // Display results in DataGrid
+                            dataGrid.ItemsSource = dataTable.DefaultView;
+                            dataGrid.Visibility = Visibility.Visible;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
